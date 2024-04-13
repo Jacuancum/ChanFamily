1a5a9a44c7290370134efb12cf23a5af9c3db176
Jacob Chan <cks2306@gmail.com>
2024-04-13 17:10:58 +0800

    cf pages added

--- scripts/hamburger2.js
+++ scripts/hamburger2.js
@@ -1,6 +1,6 @@
 const hamButton2 = document.querySelector('#submenu');
-const navigation2 = document.querySelector('.navigation');
+const navigation2 = document.querySelector('.subnavigation');
 hamButton2.addEventListener('click', () => {
-    navigation2.classList.toggle('open');
-    hamButton2.classList.toggle('open');
+    navigation2.classList.toggle('open2');
+    hamButton2.classList.toggle('open2');
 });
