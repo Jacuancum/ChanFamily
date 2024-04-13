795fa7819a2d2669cf754e1453a3a4e4e4b066aa
Jacob Chan <cks2306@gmail.com>
2024-04-13 16:07:29 +0800

    images update

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,6 +1,5 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
-const hamButton2 = document.querySelector('#submenu');
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
--- scripts/hamburger2.js
+++ scripts/hamburger2.js
@@ -1,6 +1,6 @@
 const hamButton2 = document.querySelector('#submenu');
-const navigation = document.querySelector('.navigation');
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
+const navigation2 = document.querySelector('.navigation');
+hamButton2.addEventListener('click', () => {
+    navigation2.classList.toggle('open');
     hamButton2.classList.toggle('open');
 });
